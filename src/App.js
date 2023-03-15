924940904b4b08c2e2c8118787cce1b0a0ae338a
Georgii <georgii@percents.com>
2023-03-15 16:24:32 -0400

    Done

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Home from './pages/Home/Home';
 import About from './pages/About/About';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Vans from './pages/Vans/Vans';
+import VanDetail from './pages/VanDetail/VanDetail';
 
 export default function App() {
   return (
@@ -11,6 +12,7 @@ export default function App() {
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
         <Route path='/vans' element={<Vans />} />
+        <Route path='/vans/:id' element={<VanDetail />} />
       </Routes>
     </BrowserRouter>
   );
